72563bb5ddc79e28986a4138f72f0b3077fda58e
“王俊衡” <wangjunheng23@gmail.com>
2016-12-20 21:48:55 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,18 +17,13 @@ class GroupsController < ApplicationController
 
    def create
   @group = Group.new(group_params)
-   @group.save
 
+  if @group.save
      redirect_to groups_path
- end
-
- def update
-   @group = Group.find(params[:id])
-
-   @group.update(group_params)
-
-   redirect_to groups_path, notice: "Update Success"
- end
+  else
+     render :new
+  end
+end
 
 def destroy
   @group = Group.find(params[:id])
